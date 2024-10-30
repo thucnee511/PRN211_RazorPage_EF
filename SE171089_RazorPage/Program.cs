de051aa15a5dc09bbf94dfe73da4a5d598f73c04
neru <ngocngocthuc@gmail.com>
2024-10-30 13:11:20 +0700

    first commit

--- SE171089_RazorPage/Program.cs
+++ SE171089_RazorPage/Program.cs
@@ -1,9 +1,13 @@
+using DotNetEnv;
 namespace SE171089_RazorPage
 {
     public class Program
     {
         public static void Main(string[] args)
         {
+            //Load .env file
+            Env.Load();
+            
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
